3f88b449e717c0ea9bb5a31bb5a1d64a99a6899c
Suda94 <grg_sudarshan94@hotmail.com>
2023-01-25 13:13:56 +0000

    RWD v1

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function imgslider(anything) {
+    document.querySelector('.honda').src = anything;
+
+    function changeCircleColor(color) {
+        const circle = document.querySelector('.circle');
+        circle.style.background = color;
+    }
+}
+
+
