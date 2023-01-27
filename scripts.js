134a4319bb4954db32de5d5d065a12145910b8e8
Suda94 <grg_sudarshan94@hotmail.com>
2023-01-27 12:06:33 +0000

    adding change

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
 function imgslider(anything) {
     document.querySelector('.honda').src = anything;
-
-    function changeCircleColor(color) {
-        const circle = document.querySelector('.circle');
-        circle.style.background = color;
-    }
 }
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+function toggleMenu() {
+    let menuToggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
 
 
