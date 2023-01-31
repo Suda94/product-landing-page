518cd5a51d469ac04274aa202eaaef12cb86d932
Suda94 <grg_sudarshan94@hotmail.com>
2023-01-31 13:43:18 +0000

    error fixed

--- scripts.js
+++ scripts.js
@@ -1,16 +1,22 @@
+
+
 function imgslider(anything) {
     document.querySelector('.honda').src = anything;
-}
-function changeCircleColor(color) {
-    const circle = document.querySelector('.circle');
-    circle.style.background = color;
-}
-function toggleMenu() {
-    let menuToggle = document.querySelector('.toggle');
-    let navigation = document.querySelector('.navigation')
-    menuToggle.classList.toggle('active');
-    navigation.classList.toggle('active');
-}
+
+    function changeCircleColor(color) {
+        const circle = document.querySelector('.circle');
+        circle.style.background = color;
+    }
 
 
+
+    // navigation menu on Responsive mode. 
+    function toggleMenu() {
+        let menuToggle = document.querySelector('.toggle');
+        let navigation = document.querySelector('.navigation')
+        menuToggle.classList.toggle('active');
+        navigation.classList.toggle('active');
+    }
+
+}
 
